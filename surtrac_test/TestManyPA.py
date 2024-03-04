2f023193c298aa05c9f52ce8f4f56ca77e61e8db
David <dneiman@andrew.cmu.edu>
2024-03-04 14:30:52 -0500

    Update TestManyPA.py

--- surtrac_test/TestManyPA.py
+++ surtrac_test/TestManyPA.py
@@ -31,7 +31,7 @@ for p in [0.01, 0.05, 0.25, 0.5, 0.75, 0.95, 0.99]*5:
         #print(data[0.01]["All"])
         if not p in data:
             data[p] = dict()
-        for l in ["All", "Adopters", "Non-Adopters", "All2", "Adopters2", "Non-Adopters2", "All3", "Adopters3", "Non-Adopters3", "All0", "Adopters0", "Non-Adopters0", "RNGStates"]:
+        for l in ["All", "Adopters", "Non-Adopters", "All2", "Adopters2", "Non-Adopters2", "All3", "Adopters3", "Non-Adopters3", "All0", "Adopters0", "Non-Adopters0", "Runtime", "RNGStates"]:
             if not l in data[p]:
                 data[p][l] = []
 
