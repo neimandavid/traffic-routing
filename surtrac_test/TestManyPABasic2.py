56894b4c8500c5ea08b96b30baf0e4be59157088
David <dneiman@andrew.cmu.edu>
2024-05-05 21:57:53 -0400

    Create TestManyPABasic2.py

---
+++ surtrac_test/TestManyPABasic2.py
@@ -0,0 +1,3 @@
+import runnerQueueSplit21 as runnerQueueSplit12 #Change this line if using a newer code version
+
+runnerQueueSplit12.main("PittsburghPMDataSmallerLongIn+10_fixedroutes_auto.sumocfg", 0.99, False, False, False)
