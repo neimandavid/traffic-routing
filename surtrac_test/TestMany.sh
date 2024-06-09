2fbc28324b40d611735cf3168b22a733ec48468b
David <dneiman@andrew.cmu.edu>
2024-06-09 12:39:51 -0400

    Create TestMany.sh

---
+++ surtrac_test/TestMany.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Test script"
+
+for n in {1..5};
+do
+for p in 0.01 0.05 0.25 0.5 0.75 0.95 0.99;
+do
+
+python3 runnerQueueSplit23.py PittsburghPMDataSmallerLongInMultiLaneTwoWaySmooth_fixedroutes_auto.sumocfg $p
+done
+done
