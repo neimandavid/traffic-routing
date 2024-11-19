023251d1ae7475744de59f87882bd6059a60b8fd
David <dneiman@andrew.cmu.edu>
2024-11-19 16:24:49 -0500

    Update runnerQueueSplit27.py
    Test2

--- surtrac_test/runnerQueueSplit27.py
+++ surtrac_test/runnerQueueSplit27.py
@@ -518,7 +518,8 @@ def doSurtracThread(network, simtime, light, clusters, lightphases, lastswitchti
             totalSurtracRuns += 1
         
             print("Starting NN call")
-            temp = agents["light"](nnin).detach().cpu().numpy() # Output from NN
+            temp = agents["light"](nnin) # Output from NN
+            print(temp)
             print("Ending NN call")
             if crossEntropyLoss:
                 outputNN = temp[0][1] - temp[0][0] #Stick - switch; should be <0 if switching, >0 if sticking
