931d7ea61379f716e274abd226286141b667f460
David <dneiman@andrew.cmu.edu>
2025-03-21 05:36:22 -0400

    Update runnerQueueSplit27CommsTestTest.py
    Basically nuke everything

--- surtrac_test/runnerQueueSplit27CommsTestTest.py
+++ surtrac_test/runnerQueueSplit27CommsTestTest.py
@@ -29,7 +29,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import sys
-import xml.etree.ElementTree as ET
 import os
 
 # we need to import python modules from the $SUMO_HOME/tools directory
@@ -50,98 +49,18 @@ if useLibsumo:
 if not useLibsumo:
     import traci  #To interface with SUMO simulations
 
-import sumolib #To query node/edge stuff about the network
-
-sumoconfig = None
-
-appendTrainingData = False#True
-
-isSmart = dict() #Store whether each vehicle does our routing or not
-
-netfile = "UNKNOWN_FILENAME_OOPS"
-
-def run(network, verbose = True):
-    global actualStartDict
-    startDict = dict()
-    endDict = dict()
-    delayDict = dict()
-
+def run():
     simtime = 0
 
-    while traci.simulation.getMinExpectedNumber() > 0 and (not appendTrainingData or simtime < 5000):
+    while traci.simulation.getMinExpectedNumber() > 0 :
         simtime += 1
         traci.simulationStep() #Tell the simulator to simulate the next time step
 
-        # #Decide whether new vehicles use our routing
-        # for vehicle in traci.simulation.getDepartedIDList():
-        #     isSmart[vehicle] = False
-
-        #     delayDict[vehicle] = 0#-hmetadict[goaledge][currentRoutes[vehicle][0]] #I'll add the actual travel time once the vehicle arrives
-        #     startDict[vehicle] = simtime
-
-        # #Check predicted vs. actual travel times
-        # for vehicle in traci.simulation.getArrivedIDList():
-        #     endDict[vehicle] = simtime
-
         # #Plot and print stats
         if simtime%100 == 0 or not traci.simulation.getMinExpectedNumber() > 0:
-            
-        #     #Stats
-        #     avgTime = 0
-        #     avgTime0 = 0
-        #     nCars = 0
-        #     nSmart = 0
-
-        #     for id in endDict:
-        #         if actualStartDict[id] >= 600 and actualStartDict[id] < 3000:
-        #             nCars += 1
-        #             if isSmart[id]:
-        #                 nSmart += 1
-
-        #     for id in endDict:
-        #         #Only look at steady state - ignore first and last 10 minutes of cars
-        #         if actualStartDict[id] < 600 or actualStartDict[id] >= 3000:
-        #             continue
-
-        #         ttemp = (endDict[id] - startDict[id])+delayDict[id]
-        #         avgTime += ttemp/nCars
-
-        #         #Delay0 computation (start clock at intended entrance time)
-        #         ttemp0 = (endDict[id] - actualStartDict[id])+delayDict[id]
-        #         avgTime0 += ttemp0/nCars
-
-
-            if verbose or not traci.simulation.getMinExpectedNumber() > 0 or (appendTrainingData and simtime == 5000):
-                print("\nCurrent simulation time: %f" % simtime)
-                #print("Total run time: %f" % (time.time() - tstart))
-                # print("Number of vehicles in network: %f" % traci.vehicle.getIDCount())
-                # print("Total cars that left the network: %f" % len(endDict))
-                # print("Average delay: %f" % avgTime)
-                # print("Average delay0: %f" % avgTime0)
+            print("\nCurrent simulation time: %f" % simtime)
                 
-    return []
-
-def readSumoCfg(sumocfg):
-    netfile = ""
-    roufile = ""
-    with open(sumocfg, "r") as cfgfile:
-        lines = cfgfile.readlines()
-        for line in lines:
-            if "net-file" in line:
-                data = line.split('"')
-                netfile = data[1]
-            if "route-files" in line: #This is scary - probably breaks if there's many of them
-                data = line.split('"')
-                roufile = data[1]
-    return (netfile, roufile)
-
-def main(sumoconfigin, verbose = True):
-    global lanes
-    global actualStartDict
-    global sumoconfig
-    global netfile
-
-    sumoconfig = sumoconfigin
+def main(sumoconfig, verbose = True):
 
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
@@ -150,75 +69,21 @@ def main(sumoconfigin, verbose = True):
     sumoBinary = checkBinary('sumo-gui')
     #NOTE: Script name is zeroth arg
 
-    (netfile, routefile) = readSumoCfg(sumoconfig)
-
-    network = sumolib.net.readNet(netfile)
-    net = network
-
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
     if useLibsumo:
+        print("Using libsumo, yay!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         traci.load(["-c", sumoconfig,
                                 #"--additional-files", "additional_autogen.xml",
                                 "--no-step-log", "true",
                                 "--log", "LOGFILE", "--xml-validation", "never", "--start", "--quit-on-end"])
     else:
-        try:
-            traci.start([sumoBinary, "-c", sumoconfig,
-                                    #"--additional-files", "additional_autogen.xml",
-                                    "--no-step-log", "true",
-                                    "--log", "LOGFILE", "--xml-validation", "never", "--start", "--quit-on-end"], label="main")
-            #Second simulator for running tests. No GUI
-            #traci.start([sumoBinary, "-c", sumoconfig, #GUI in case we need to debug
-            traci.start([checkBinary('sumo'), "-c", sumoconfig, #No GUI
-                                    #"--additional-files", "additionalrouting_autogen.xml",
-                                    "--start", "--no-step-log", "true",
-                                    "--xml-validation", "never", "--quit-on-end",
-                                    "--step-length", "1"], label="test")
-            dontBreakEverything()
-        except:
-            #Worried about re-calling this without old main instance being removed
-            if not useLibsumo:
-                traci.switch("main")
-            traci.load([ "-c", sumoconfig,
-                                    "--additional-files", "additional_autogen.xml",
-                                    "--no-step-log", "true",
-                                    #"--time-to-teleport", "-1",
-                                    "--log", "LOGFILE", "--xml-validation", "never", "--start", "--quit-on-end"])
-            if not useLibsumo:
-                traci.switch("test")
-            traci.load([ "-c", sumoconfig,
-                                    "--additional-files", "additionalrouting_autogen.xml",
-                                    "--no-step-log", "true",
-                                    #"--time-to-teleport", "-1",
-                                    "--log", "LOGFILE", "--xml-validation", "never", "--start", "--quit-on-end"])
-            dontBreakEverything()
+        traci.start([sumoBinary, "-c", sumoconfig,
+                                #"--additional-files", "additional_autogen.xml",
+                                "--no-step-log", "true",
+                                "--log", "LOGFILE", "--xml-validation", "never", "--start", "--quit-on-end"], label="main")
 
-
-    #Parse route file to get intended departure times (to account for delayed SUMO insertions due to lack of space)
-    #Based on: https://www.geeksforgeeks.org/xml-parsing-python/
-    # create element tree object
-    tree = ET.parse(routefile)
-
-    # get root element
-    root = tree.getroot()
-
-    actualStartDict = dict()
-    # iterate news items
-    for item in root.findall('./vehicle'):
-        actualStartDict[item.attrib["id"]] = float(item.attrib["depart"])
-    for item in root.findall('./trip'):
-        actualStartDict[item.attrib["id"]] = float(item.attrib["depart"])
-
-    outdata = run(network, verbose)
-
-#Magically makes the vehicle lists stop deleting themselves somehow???
-def dontBreakEverything():
-    if not useLibsumo:
-        traci.switch("test")
-        traci.simulationStep()
-        traci.switch("main")
-
+    outdata = run()
 
 # this is the main entry point of this script
 if __name__ == "__main__":
