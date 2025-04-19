fd3fd2a89dee23bfc89708c2b12fc249522b99df
David <dneiman@andrew.cmu.edu>
2025-04-19 03:34:30 -0400

    Update TestMany.sh

--- surtrac_test/TestMany.sh
+++ surtrac_test/TestMany.sh
@@ -7,6 +7,6 @@ do
 for n in {1..5};
 do
 
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+10New_fixedroutes_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+10Newer_fixedroutes_auto.sumocfg $p
 done
 done
