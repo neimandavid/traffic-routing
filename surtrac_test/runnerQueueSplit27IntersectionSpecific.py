25997513e9b5a77786798b3f6d0d425d0032c015
David <dneiman@andrew.cmu.edu>
2025-05-07 23:44:45 -0400

    Update runnerQueueSplit27IntersectionSpecific.py

--- surtrac_test/runnerQueueSplit27IntersectionSpecific.py
+++ surtrac_test/runnerQueueSplit27IntersectionSpecific.py
@@ -302,7 +302,7 @@ def consolidateClusters(clusters):
     #Sort the cluster list
     pq = []
     for cluster in clusters:
-        heappush(pq, cluster, cluster["arrival"])
+        heappush(pq, (cluster["arrival"], cluster))
     newclusters = []
     while len(pq) > 0:
         newclusters.append(heappop(pq))
