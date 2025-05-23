e6f9f44ccbc3708c3a807e96b97a4c6962637dbf
David <dneiman@andrew.cmu.edu>
2025-05-23 15:25:47 -0400

    Fix hyperparameters and filenames

--- surtrac_test/TestMany.sh
+++ surtrac_test/TestMany.sh
@@ -7,6 +7,6 @@ do
 for n in {1..5};
 do
 
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+10Newestest_fixedroutes_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestest_fixedroutes_auto.sumocfg $p
 done
 done
