27bd8d6c6894803d75fbbf4e769db0e7ecccddf4
David <dneiman@andrew.cmu.edu>
2025-05-26 09:38:34 -0400

    Automated hyperparameter tuning

---
+++ surtrac_test/TestDiscount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Test script"
+
+for d in 0 0.2 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.7 0.8 1;
+do
+for n in 1..5
+do
+
+python3 runnerQueueSplit27IntersectionSpecificDiscountTest.py PittsburghPMDataSmallerLongIn+15Newestest_fixedroutes_auto.sumocfg 0.01 False False $d
+done
+done
