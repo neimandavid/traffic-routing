b028696ba8b8ed46221abe4a02700524110fd894
David <dneiman@andrew.cmu.edu>
2025-06-09 13:14:13 -0400

    Use new Pittsburgh

--- surtrac_test/TestMany.sh
+++ surtrac_test/TestMany.sh
@@ -7,7 +7,7 @@ do
 for n in {1..5};
 do
 
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestest_fixedroutes_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestester_fixedroutes_auto.sumocfg $p
 done
 done
 
@@ -16,6 +16,6 @@ do
 for p in 0.25 0.5 0.75 0.95 0.99;
 do
 
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestest_fixedroutes_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestester_fixedroutes_auto.sumocfg $p
 done
 done
