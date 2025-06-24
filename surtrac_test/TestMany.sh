99bba974aa9b9d7b6e5857388a07345ed005fc07
David <dneiman@andrew.cmu.edu>
2025-06-23 23:48:47 -0400

    Hopefully good?

--- surtrac_test/TestMany.sh
+++ surtrac_test/TestMany.sh
@@ -2,20 +2,11 @@
 
 echo "Test script"
 
-for p in 0.01 0.05;
-do
 for n in {1..5};
 do
-
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestester_fixedroutes_auto.sumocfg $p
-done
-done
-
-for n in {1..5};
-do
-for p in 0.25 0.5 0.75 0.95 0.99;
+for p in 0.01 0.05 0.25 0.5 0.75 0.95 0.99;
 do
 
-python3 runnerQueueSplit27IntersectionSpecific.py PittsburghPMDataSmallerLongIn+15Newestester_fixedroutes_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecificGood.py blocks51h_auto.sumocfg $p
 done
 done
