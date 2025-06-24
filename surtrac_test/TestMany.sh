0564a9293ff6c6837ee5b62d6f17ac498d84cc93
David <dneiman@andrew.cmu.edu>
2025-06-23 23:52:15 -0400

    Update TestMany.sh

--- surtrac_test/TestMany.sh
+++ surtrac_test/TestMany.sh
@@ -7,6 +7,6 @@ do
 for p in 0.01 0.05 0.25 0.5 0.75 0.95 0.99;
 do
 
-python3 runnerQueueSplit27IntersectionSpecificGood.py blocks51h_auto.sumocfg $p
+python3 runnerQueueSplit27IntersectionSpecificGood.py blocks51hNewestester_auto.sumocfg $p
 done
 done
