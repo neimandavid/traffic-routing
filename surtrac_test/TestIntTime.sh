8b3f66ed4ae502f66478c93c699a7ffa9593b082
neimandavid <dneiman@andrew.cmu.edu>
2025-08-26 20:12:02 -0400

    Prep to recalibrate intersectionTime

---
+++ surtrac_test/TestIntTime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+echo "Test script"
+
+for n in {1..5}
+do
+for d in 0 1 0.5 2 0.4 0.6 1.5 0.1 0.7 0.3 0.2 0.8 0.9;
+do
+
+python3 runnerQueueSplit31TestIntTime.py PittsburghPMDataSmallerLongInNewestester_fixedroutes_auto.sumocfg 0.01 False False $d
+done
+done
