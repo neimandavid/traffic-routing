d5923f08235a5d453be852041199f89df9348e3d
neimandavid <dneiman@andrew.cmu.edu>
2025-10-25 01:31:47 -0400

    Update runnerQueueSplit31.py
    Prep for FTPs

--- surtrac_test/runnerQueueSplit31.py
+++ surtrac_test/runnerQueueSplit31.py
@@ -104,15 +104,15 @@ routingSimUsesSUMO = True #Only switch this if we go back to custom routing simu
 mainSurtracFreq = 1 #Recompute Surtrac schedules every this many seconds in the main simulation (technically a period not a frequency). Use something huge like 1e6 to disable Surtrac and default to fixed timing plans.
 routingSurtracFreq = 1 #Recompute Surtrac schedules every this many seconds in the main simulation (technically a period not a frequency). Use something huge like 1e6 to disable Surtrac and default to fixed timing plans.
 recomputeRoutingSurtracFreq = 1 #Maintain the previously-computed Surtrac schedules for all vehicles routing less than this many seconds in the main simulation. Set to 1 to only reuse results within the same timestep. Does nothing when reuseSurtrac is False.
-disableSurtracComms = False#True #Speeds up code by having Surtrac no longer predict future clusters for neighboring intersections
+disableSurtracComms = True #Speeds up code by having Surtrac no longer predict future clusters for neighboring intersections
 predCutoffMain = 30 #Surtrac receives communications about clusters arriving this far into the future in the main simulation
 predCutoffRouting = 0 #Surtrac receives communications about clusters arriving this far into the future in the routing simulations
 predDiscount = 1 #Multiply predicted vehicle weights by this because we're not actually sure what they're doing. 0 to ignore predictions, 1 to treat them the same as normal cars.
 intersectionTime = 0.5 #Gets added to arrival time for predicted clusters to account for vehicles needing time to go through intersections. Should account for sult maybe. Do I need to be smarter to handle turns?
 
-testNNdefault = True #Uses NN over Dumbtrac for light control if both are true
+testNNdefault = False #Uses NN over Dumbtrac for light control if both are true
 noNNinMain = True
-testDumbtrac = False #If true, overrides Surtrac with Dumbtrac (FTP or actuated control) in simulations and training data (if appendTrainingData is also true)
+testDumbtrac = True #If true, overrides Surtrac with Dumbtrac (FTP or actuated control) in simulations and training data (if appendTrainingData is also true)
 FTP = True #If false, and testDumbtrac = True, runs actuated control instead of fixed timing plans. If true, runs fixed timing plans (should now be same as SUMO defaults)
 resetTrainingData = False#True
 appendTrainingData = False#True
