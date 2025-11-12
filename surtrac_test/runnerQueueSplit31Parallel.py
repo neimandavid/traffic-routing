cbc8e6f0db5d10402a0b115f95b407e51887491f
neimandavid <dneiman@andrew.cmu.edu>
2025-11-12 13:05:28 -0500

    Update runnerQueueSplit31Parallel.py
    Readd the 1e6 on the autoswitch, might have been the problem

--- surtrac_test/runnerQueueSplit31Parallel.py
+++ surtrac_test/runnerQueueSplit31Parallel.py
@@ -547,7 +547,7 @@ def doSurtracThread(simtime, light, clusters, lightphases, lastswitchtimes, inRo
                 if light in bestschedules:
                     temp = pickle.loads(pickle.dumps(bestschedules[light][7]))
                 else:
-                    temp = [0]
+                    temp = [0, 1e6]
                 temp[0] = dur
                 bestschedules[light] = [None, None, None, None, None, None, None, temp]
                 return
@@ -557,7 +557,7 @@ def doSurtracThread(simtime, light, clusters, lightphases, lastswitchtimes, inRo
                 if light in bestschedules:
                     temp = pickle.loads(pickle.dumps(bestschedules[light][7]))
                 else:
-                    temp = [0]
+                    temp = [0, 1e6]
                 temp[0] = dur
                 bestschedules[light] = [None, None, None, None, None, None, None, temp]
                 return
