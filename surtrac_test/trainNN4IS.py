3d316ffc701d4216fb091b3f3728c8107ec95fcc
neimandavid <dneiman@andrew.cmu.edu>
2025-11-11 21:19:06 -0500

    Update trainNN4IS.py
    CE loss Excel print should actually work this time

--- surtrac_test/trainNN4IS.py
+++ surtrac_test/trainNN4IS.py
@@ -386,7 +386,7 @@ def dumpTrainingData(trainingdata):
                     if row == 2:
                         sheets[light].cell(1, col, "NN Output")
                     if len(batch) > 2:
-                        for entry in batch[2][linenum]:
+                        for entry in batch[2][linenum].numpy()[0][0]:
                             sheets[light].cell(row, col, float(entry))
                             col += 1
                         #sheets[light].cell(row, col, float(batch[2][linenum])) #TODO this is wrong for cross-entropy loss
